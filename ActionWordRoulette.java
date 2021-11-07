6ec7d6be8d3c514bb6bd7ae044912e601e0bb462
Roger Cheng <cheng2themax@gmail.com>
2021-11-07 12:10:34 -0500

    Added more to list of experiences

--- ActionWordRoulette.java
+++ ActionWordRoulette.java
@@ -6,8 +6,8 @@ public class ActionWordRoulette {
         String[] actionWords = {"Chaired","Controlled","Coordinated","Executed","Headed","Operated","Orchestrated","Organized","Oversaw","Planned","Produced","Programmed","Administered","Built","Charted","Created","Designed","Developed","Devised","Founded","Engineered","Established","Formalized","Formed","Formulated","Implemented","Incorporated","Initiated","Instituted","Introduced","Launched","Pioneered","Spearheaded","Conserved","Consolidated","Decreased","Deducted","Diagnosed","Lessened","Reconciled","Reduced","Yielded","Accelerated","Achieved","Advanced","Amplified","Boosted","Capitalized","Delivered","Enhanced","Expanded","Expedited","Furthered","Gained","Generated","Improved","Lifted","Maximized","Outpaced","Stimulated","Sustained","Centralized","Clarified","Converted","Customized","Influenced","Integrated","Merged","Modified","Overhauled","Redesigned","Refined","Refocused","Rehabilitated","Remodeled","Reorganized","Replaced","Restructured","Revamped","Revitalized","Simplified","Standardized","Streamlined","Strengthened","Updated","Upgraded","Transformed","Aligned","Cultivated","Directed","Enabled","Facilitated","Fostered","Guided","Hired","Inspired","Mentored","Mobilized","Motivated","Recruited","Regulated","Shaped","Supervised","Taught","Trained","Unified","United","Acquired","Forged","Navigated","Negotiated","Partnered","Secured","Advised","Advocated","Arbitrated","Coached","Consulted","Educated","Fielded","Informed","Resolved","Analyzed","Assembled","Assessed","Audited","Calculated","Discovered","Evaluated","Examined","Explored","Forecasted","Identified","Interpreted","Investigated","Mapped","Measured","Qualified","Quantified","Surveyed","Tested","Tracked","Authored","Briefed","Campaigned","Co-authored","Composed","Conveyed","Convinced","Corresponded","Counseled","Critiqued","Defined","Documented","Edited","Illustrated","Lobbied","Persuaded","Promoted","Publicized","Reviewed","Authorized","Blocked","Delegated","Dispatched","Enforced","Ensured","Inspected","Itemized","Monitored","Screened","Scrutinized","Verified","Attained","Awarded","Completed","Demonstrated","Earned","Exceeded","Outperformed","Reached","Showcased","Succeeded","Surpassed","Targeted"};
         HashMap<String,Integer> word_values = new HashMap<>();
         int i = 0;
-        while(i<10){
-            int weight = (int)(Math.random()*10);
+        while(i<5){
+            int weight = (int)(Math.random()*100);
             int randIndex = (int)(Math.random()*actionWords.length);
             if (!word_values.containsKey(actionWords[randIndex])){
             word_values.put(actionWords[randIndex], weight);
@@ -15,8 +15,15 @@ public class ActionWordRoulette {
             }
         }
         
-        for (String word: word_values.keySet()){
-            System.out.println("Word:" + word + "  Value: " + word_values.get(word));
+        // for (String word: word_values.keySet()){
+        //     System.out.println("Word:" + word + "  Value: " + word_values.get(word));
+        // }
+        // for (int x = 0 ; x< actionWords.length; x++){
+        //     System.out.println(actionWords[x]);
+        // }
+        
+        for (int z = 0; z < actionWords.length; z++){
+            System.out.println(actionWords[z]);
         }
 
     }
--- ResumeRoulette.java
+++ ResumeRoulette.java
@@ -1,34 +1,58 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+// DRIVER CLASS 
 public class ResumeRoulette {
 
-    public static String[] scienceExp = { "Research at Feinstein Institute", "Published in Nature Magazine" , "McCormick Intern"};
+    public static String[] scienceExp = { "Research at Feinstein Institute", "Published in Nature Magazine",
+            "McCormick Intern", "Intern at Cold Spring Harbor" };
     public static String[] scienceExp1 = { "CHE 131 TA", "Science Olympiad Member" };
-    public static String[] scienceExp2 = { "Interned at Memorial Sloan Kettering" };
+    public static String[] scienceExp2 = { "Interned at Memorial Sloan Kettering", "STEM Cell Engineering VIP", "Approaches for Tissue Engineered Kidneys VIP", "The Brain Team VIP" };
 
     public static String[] techExp = { "Zebra Technologies Intern", "Amazon SDE Internship", "MasterCard Internship",
-            "Morgan Stanley Intern", "Solutions Architect Intern at Amazon" };
-    public static String[] techExp1 = { "SBU Hacks participant" };
+            "Morgan Stanley Intern", "Solutions Architect Intern at Amazon", "Google SWE Intern",
+            "Product Manager Intern at Facebook", "MongoDB Intern", "SalesForce Intern" };
+    public static String[] techExp1 = { "SBU Hacks participant", "VIP Automotive Ethics", "VIP Web Genomics" };
     public static String[] techExp2 = { "CSE 101 TA", "WiCs Mentee" };
 
-    public static String[] engineeringExp = { "PSEG Intern", "SBU MotorSports Member", "Boeing Intern", "Interned at NASA" };
-    public static String[] engineeringExp1 = {"Solar Racing E-Board member" };
-    public static String[] engineeringExp2 = {};
+    public static String[] engineeringExp = { "PSEG Intern", "SBU MotorSports Member", "Boeing Intern",
+            "Interned at NASA" };
+    public static String[] engineeringExp1 = { "Solar Racing E-Board member", "PFizer BioTech I", "Regeneron Intern" };
+    public static String[] engineeringExp2 = { "Physics tutor", "ASML intern", "Long Island Waste Management VIP", "AMMAT VIP" };
 
-    public static String[] mathExp = { "Putnam Math Competition Runner up", "USAMO Qualifier" };
+    public static String[] mathExp = { "HRT Intern", "2 Sigma Quant Intern", "NVIDIA Data Science Intern", "Roblox Data Science Intern" };
     public static String[] mathExp1 = { "AIME Qualifier", "TA AMS 301", "Math Research", "Math Fair Winner" };
     public static String[] mathExp2 = { "Math Circle Volunteer" };
 
-    public static String[] miscExp = { "Captain of Varsity Basketball Team", "E-Board member of SASE", "SASE Mentee" };
+    public static String[] miscExp = { "Captain of Varsity Basketball Team", "E-Board member of SASE", "SASE Mentee",
+            "Chess Club E-Board", "Intramural Volleyball Captain", "Starbucks Barista", "Walmart Cashier" };
 
+    // UNIVERSAL SKILLS APPLICABLE TO ALL STEM
     public static String[] hardSkills = { "Python", "Java", "JavaScript", "LaTex", "Git", "Microsoft Excel",
             "Microsoft Word", "Microsoft Powerpoint", "AutoCAD", "R", "MiniTab", "Linux", "Docker", "Adobe Photoshop" };
 
+    // SCIENCE SPECIAL SKILLS x PROJECTS
+    // public static String[] labSkills = {};
+    public static String[] scienceProjects = {};
+
+    // TECH SPECIAL SKILLS x PROJECTS
     public static String[] csFrameworks = { "Angular", "MERN stack", "CI/CD", "Android Frameworks", "C++", "C#",
             "Ocaml", "GoLang", "Rust", "TypeScript", "Ruby", "Scala", "Scheme", "Lisp", "MongoDB", "React", "AWS",
             "Kubernetes", "Microsoft Azure", "Google Cloud", "Tailwind", "Gatsby", "Design Patterns", "System Design",
             "Apache", "Nginx" };
+    public static String[] techProjects = { "Automated Chatbot", "Sudoku Solver", "Self-driving Raspberry Pi Car",
+            "Graph Algorithm Visualizer", "Discord Bot" };
+
+    // ENGINEERING SPECIAL SKILLS x PROJECTS
+    public static String[] engineerSkills = {};
+    public static String[] engineeringProjects = {};
+
+    // MATHEMATICS SPECIAL SKILLS x PROJECTS
+    public static String[] mathSkills = {};
+    public static String[] mathProjects = {};
+
+    // CERTIFICATIONS
+    public static String[] certifications = { "First-Aid Training" };
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
@@ -38,9 +62,9 @@ public class ResumeRoulette {
         System.out.println("Input Major:  AMS BCH BIO BME CHE CIV CME CSE ECE ESE ISE MAT MEC PHY TSM ");
         String major = in.next();
         System.out.println("Profile Name: " + personName);
-        System.out.println("GPA:" + (3 + (Math.random())));
+        System.out.println("GPA:" + String.format("%.2f", (3 + (Math.random()))));
 
-        //PRINT OUT THE DISTINCT PROFILE TYPES 
+        // PRINT OUT THE DISTINCT PROFILE TYPES
 
         // SCIENCE
         if (major.equals("CHE") || major.equals("BIO") || major.equals("BCH")) {
@@ -51,17 +75,17 @@ public class ResumeRoulette {
             System.out.println("Skills:" + technology());
         }
         // ENGINEERING
-        else if (major.equals("BME") || major.equals("CIV") || major.equals("CME") || major.equals("ECE")) {
+        else if (major.equals("BME") || major.equals("CIV") || major.equals("CME") || major.equals("ECE") || major.equals("MEC") || major.equals("ESE")) {
             System.out.println("Skills:" + engineering());
         }
         // MATHEMATICS
         else {
             System.out.println("Skills:" + mathematics());
         }
-
+        System.out.println("Miscellaneous Experiences: " + randElem(miscExp));
     }
 
-    // CHE BIO BCH 
+    // CHE BIO BCH
     public static String science() {
         ArrayList<String> resume = generateSkills(hardSkills);
         System.out.println("Experience 1: " + randElem(scienceExp));
@@ -70,7 +94,6 @@ public class ResumeRoulette {
         return resume.toString();
     }
 
-        
     public static String technology() {
         // GENERATE SKILLS
         ArrayList<String> resume = generateSkills(hardSkills);
@@ -112,7 +135,7 @@ public class ResumeRoulette {
         resume.add(newSkill);
         int skillCount = 1;
 
-        int maxSkill = (int) (Math.random() * (skillArr.length - 5)) + 5; //minimum 5 
+        int maxSkill = (int) (Math.random() * (skillArr.length - 5)) + 5; // minimum 5
         while (skillCount < maxSkill) {
             // reset what newSkill is
             newSkill = randElem(skillArr);
@@ -124,9 +147,9 @@ public class ResumeRoulette {
         return resume;
     }
 
-    //Random arrayElement to select experiences 
-    public static String randElem(String [] arr){
-        return arr[(int) (Math.random()*arr.length)];
+    // Random arrayElement to select experiences
+    public static String randElem(String[] arr) {
+        return arr[(int) (Math.random() * arr.length)];
     }
- 
+
 }
